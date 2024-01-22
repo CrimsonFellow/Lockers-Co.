e8360ec7dabe3b4dcea1287fb8ba1a8ce61ddbc6
CrimsonFellow <eldrigedongallego@gmail.com>
2024-01-22 09:42:50 +0200

    Welcome commit

---
+++ LockersCo/src/backend/WelcomeMenu.java
@@ -0,0 +1,8 @@
+package backend;
+
+public class WelcomeMenu {
+    public static void showWelcomeScreen() {
+        System.out.println("Welcome to File Manager");
+        System.out.println("");
+        }
+}
